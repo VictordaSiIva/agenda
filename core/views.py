ab98527cbb5eb61f9ddaafab9f146e2fd1f8ff42
Victor da Silva <victor10019@hotmail.com>
2019-04-07 22:12:33 -0300

    adicionado funções de alteração e exclusao de dados

--- core/views.py
+++ core/views.py
@@ -35,6 +35,10 @@ def lista_eventos(request):
     return render(request, 'agenda.html', dados)
 
 def evento(request):
+    id_evento = request.GET.get('id')
+    dados = {}
+    if id_evento:
+         dados['evento'] = Evento.objects.get(id=id_evento)
     return render(request, 'evento.html')
 
 @login_required(login_url='/login/')
@@ -44,8 +48,25 @@ def submit_evento(request):
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
-        Evento.objects.create(titulo=titulo,
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            evento = Evento.objects.get(id=id_evento)
+            if evento.usuario == usuario:
+                evento.titulo = titulo
+                evento.descricao = descricao
+                evento.data_evento = data_evento
+                evento.save()
+        else:
+            Evento.objects.create(titulo=titulo,
                               data_evento = data_evento,
                               descricao=descricao,
                               usuario=usuario)
+    return redirect('/')
+
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
     return redirect('/')
